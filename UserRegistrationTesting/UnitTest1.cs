95e9b0655924a8aa14ea1f987ef852719084c44a
im4kshay <akshaysayre7@gmail.com>
2022-04-29 10:52:49 +0530

    Using Lambda function to validate

--- UserRegistrationTesting/UnitTest1.cs
+++ UserRegistrationTesting/UnitTest1.cs
@@ -63,63 +63,6 @@ namespace UserRegistrationTesting
             //Assert
             Assert.IsTrue(result);
         }
-        //Sad Test Cases(Test Cases Fail The Entry)
-        [TestMethod]
-        public void GivenUserFistName_WhenValidate_ShouldReturnFalse()
-        {
-            //Arrange
-            UserRegistrationValidation user = new UserRegistrationValidation();
-            string firstName = "akshay";
-            //Act
-            bool result = user.ValidateFirstName(firstName);
-            //Assert
-            Assert.IsFalse(result);
-        }
-        [TestMethod]
-        public void GivenUserLastName_WhenValidate_ShouldReturnFalse()
-        {
-            //Arrange
-            UserRegistrationValidation user = new UserRegistrationValidation();
-            string lastName = "sayre";
-            //Act
-            bool result = user.ValidateFirstName(lastName);
-            //Assert
-            Assert.IsFalse(result);
-        }
-        [TestMethod]
-        public void GivenEmailId_WhenValidate_ShouldReturnFalse()
-        {
-            //Arrange
-            UserRegistrationValidation user = new UserRegistrationValidation();
-            string email = "akshaysayregmail.com";
-            //Act
-            bool result = user.ValidateEmail(email);
-            //Assert
-            Assert.IsFalse(result);
-        }
-        [TestMethod]
-        public void GivenMobileNumber_WhenValidate_ShouldReturnFalse()
-        {
-            //Arrange
-            UserRegistrationValidation user = new UserRegistrationValidation();
-            string mobileNumber = "91 95876006";
-            //Act
-            bool result = user.ValidateMobileNumber(mobileNumber);
-            //Assert
-            Assert.IsFalse(result);
-        }
-        [TestMethod]
-        public void GivenPassword_WhenValidate_ShouldReturnFalse()
-        {
-            //Arrange
-            UserRegistrationValidation user = new UserRegistrationValidation();
-            string password = "akshay@#123";
-            //Act
-            bool result = user.ValidatePassword(password);
-            //Assert
-            Assert.IsFalse(result);
-        }
-        //Uc-11 Validate Multiple Emails
         [TestMethod]
         [DataRow("abc@yahoo.com")]
         [DataRow("abc-100@yahoo.com")]
